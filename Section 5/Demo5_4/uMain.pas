4b6683d130fbf9a2c871a618b4c02efeacfcc4a6
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-07-26 17:04:41 +0500

    Update uMain.pas

--- Section 5/Demo5_4/uMain.pas
+++ Section 5/Demo5_4/uMain.pas
@@ -7,12 +7,13 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, IPPeerClient,
   FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
   FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
-  REST.Response.Adapter, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
-  REST.Client, Data.Bind.Components, Data.Bind.ObjectScope, System.Rtti,
-  FMX.Grid.Style, Fmx.Bind.Grid, System.Bindings.Outputs, Fmx.Bind.Editors,
-  Data.Bind.EngExt, Fmx.Bind.DBEngExt, Data.Bind.Grid,
+  System.Rtti, FMX.Grid.Style, Fmx.Bind.Grid, System.Bindings.Outputs,
+  Fmx.Bind.Editors, Data.Bind.Controls, Data.Bind.EngExt, Fmx.Bind.DBEngExt,
+  Data.Bind.Components, FMX.Layouts, Fmx.Bind.Navigator, Data.Bind.Grid,
   FMX.Controls.Presentation, FMX.ScrollBox, FMX.Grid, Data.Bind.DBScope,
-  FMX.StdCtrls, FMX.Memo, FMX.Edit;
+  Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client, REST.Response.Adapter,
+  REST.Client, Data.Bind.ObjectScope, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.ListView, FMX.Memo;
 
 type
   TForm1 = class(TForm)
@@ -22,15 +23,11 @@ type
     RESTResponseDataSetAdapter1: TRESTResponseDataSetAdapter;
     FDMemTable1: TFDMemTable;
     BindSourceDB1: TBindSourceDB;
-    GridBindSourceDB1: TGrid;
-    LinkGridToDataSourceBindSourceDB1: TLinkGridToDataSource;
     BindingsList1: TBindingsList;
+    ListView1: TListView;
     Memo1: TMemo;
-    Edit1: TEdit;
-    Label1: TLabel;
-    LinkPropertyToFieldText: TLinkPropertyToField;
+    LinkListControlToField1: TLinkListControlToField;
     LinkControlToField1: TLinkControlToField;
-    LinkControlToField2: TLinkControlToField;
     procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
