925620d3d90e951b80946607af92dcb7d4fd4862
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-07-28 13:57:30 +0500

    Add files via upload

---
+++ Section 1/4/Demo1_4.dpr
@@ -0,0 +1,16 @@
+program Demo1_4;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uMain in 'uMain.pas' {Form1},
+  Unit2 in 'Unit2.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
